fb766bd85a302aa78247641f56b642a0f9068e41
Mahad Ahmed <i200426@nu.edu.pk>
2024-03-29 09:36:19 +0500

    Initial commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,113 @@
+
+    import React from 'react';
+    import './styles.css'; // Import your stylesheet
+
+    const LoginPage = () => {
+    return (
+        <body>
+<div class="desktop-screen">
+<div id="id_d_245_305">
+<div id="id_d_245_306">
+<div id="id_d_245_307">
+<div id="id_d_245_308">
+<div id="id_d_I245_308_24_1261">
+</div>
+</div>
+<p id="id_d_245_309">FastLink</p>
+</div>
+<p id="id_d_245_310">Sign In</p>
+<div id="id_d_245_311">
+<div id="id_d_245_312">
+<p id="id_d_I245_312_24_2086">Label</p>
+<input id="id_d_I245_312_24_2087" type="text"/>
+<p id="id_d_I245_312_24_2087_24_2090">placeholder</p>
+</div>
+<div id="id_d_245_313">
+<p id="id_d_I245_313_24_2086">Label</p>
+<input id="id_d_I245_313_24_2087" type="text"/>
+<p id="id_d_I245_313_24_2087_24_2090">placeholder</p>
+</div>
+</div>
+<button id="id_d_245_314">
+<p id="id_d_I245_314_6_1037">Create Ticket</p>
+</button>
+<p id="id_d_245_315">or</p>
+<button id="id_d_245_316">
+<p id="id_d_245_317">Sign Up</p>
+</button>
+</div>
+</div>
+</div>
+<div class="tablet-screen">
+<div id="id_t_245_305">
+<div id="id_t_245_306">
+<div id="id_t_245_307">
+<div id="id_t_245_308">
+<div id="id_t_I245_308_24_1261">
+</div>
+</div>
+<p id="id_t_245_309">FastLink</p>
+</div>
+<p id="id_t_245_310">Sign In</p>
+<div id="id_t_245_311">
+<div id="id_t_245_312">
+<p id="id_t_I245_312_24_2086">Label</p>
+<input id="id_t_I245_312_24_2087" type="text"/>
+<p id="id_t_I245_312_24_2087_24_2090">placeholder</p>
+</div>
+<div id="id_t_245_313">
+<p id="id_t_I245_313_24_2086">Label</p>
+<input id="id_t_I245_313_24_2087" type="text"/>
+<p id="id_t_I245_313_24_2087_24_2090">placeholder</p>
+</div>
+</div>
+<button id="id_t_245_314">
+<p id="id_t_I245_314_6_1037">Create Ticket</p>
+</button>
+<p id="id_t_245_315">or</p>
+<button id="id_t_245_316">
+<p id="id_t_245_317">Sign Up</p>
+</button>
+</div>
+</div>
+</div>
+<div class="mobile-screen">
+<div id="id_m_245_392">
+<div id="id_m_245_393">
+<div id="id_m_245_394">
+<div id="id_m_245_395">
+<div id="id_m_245_396">
+<div id="id_m_I245_396_24_1261">
+</div>
+</div>
+</div>
+<p id="id_m_245_397">FastLink</p>
+</div>
+<p id="id_m_245_398">Sign In</p>
+<div id="id_m_245_399">
+<div id="id_m_245_400">
+<p id="id_m_I245_400_24_2086">Label</p>
+<input id="id_m_I245_400_24_2087" type="text"/>
+<p id="id_m_I245_400_24_2087_24_2090">placeholder</p>
+</div>
+<div id="id_m_245_401">
+<p id="id_m_I245_401_24_2086">Label</p>
+<input id="id_m_I245_401_24_2087" type="text"/>
+<p id="id_m_I245_401_24_2087_24_2090">placeholder</p>
+</div>
+</div>
+<button id="id_m_245_402">
+<p id="id_m_I245_402_6_1037">Create Ticket</p>
+</button>
+<p id="id_m_245_403">or</p>
+<button id="id_m_245_404">
+<p id="id_m_245_405">Sign Up</p>
+</button>
+</div>
+</div>
+</div>
+</body>
+    );
+    };
+    export default LoginPage;
+    
